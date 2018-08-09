451fd0152e5a9fb0016af7791b5e19c99657dd5d
zhezhang91 <zz188659@gmail.com>
2018-08-09 17:07:12 -0400

    store token locally

--- src/index.js
+++ src/index.js
@@ -1,27 +1,35 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
-import {BrowserRouter} from 'react-router-dom';
-import {Provider} from 'react-redux';
-import {createStore, applyMiddleware, compose, combineReducers} from 'redux';
-import burgerBuilderReducer from './store/reducers/burgerBuilder';
-import orderReducer from './store/reducers/order';
-import thunk from 'redux-thunk';
-
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import registerServiceWorker from "./registerServiceWorker";
+import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware, compose, combineReducers } from "redux";
+import burgerBuilderReducer from "./store/reducers/burgerBuilder";
+import orderReducer from "./store/reducers/order";
+import authReducer from "./store/reducers/auth";
+import thunk from "redux-thunk";
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 const rootReducer = combineReducers({
-    burgerBuilder: burgerBuilderReducer,
-    order: orderReducer
+  burgerBuilder: burgerBuilderReducer,
+  order: orderReducer,
+  auth: authReducer
 });
 
-const store = createStore(rootReducer, composeEnhancers(
-    applyMiddleware(thunk)
-));
+const store = createStore(
+  rootReducer,
+  composeEnhancers(applyMiddleware(thunk))
+);
 
-
-ReactDOM.render(<Provider store={store}><BrowserRouter><App /></BrowserRouter></Provider>, document.getElementById('root'));
+ReactDOM.render(
+  <Provider store={store}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>,
+  document.getElementById("root")
+);
 registerServiceWorker();
--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -1,46 +1,52 @@
-import * as actionTypes from './actionTypes';
-import axios from 'axios';
+import * as actionTypes from "./actionTypes";
+import axios from "axios";
 
 const APIKey = "AIzaSyBGPw3AGtZmEaTyy2GuTr5hjlHwlO_jDew";
 
 export const authStart = () => {
-    return {
-        type: actionTypes.AUTH_START
-    }
+  return {
+    type: actionTypes.AUTH_START
+  };
 };
 
-export const authSuccess = (authData) => {
-    return {
-        type: actionTypes.AUTH_SUCCESS,
-        authData: authData
-    }
+export const authSuccess = (token, userId) => {
+  return {
+    type: actionTypes.AUTH_SUCCESS,
+    idToken: token,
+    userId: userId
+  };
 };
 
-export const authFailed = (error) => {
-    return {
-        type: actionTypes.AUTH_FAILED,
-        error: error
-    }
+export const authFailed = error => {
+  return {
+    type: actionTypes.AUTH_FAILED,
+    error: error
+  };
 };
 
-export const auth = (email, password) => {
-    return dispatch => {
-        dispatch(authStart());
-        const authData = {
-            email: email,
-            password: password,
-            returnSecureToken: true
-        };
-
-        axios.post('https://www.googleapis.com/identitytoolkit/v3/relyingparty/signupNewUser?key=AIzaSyBGPw3AGtZmEaTyy2GuTr5hjlHwlO_jDew' , authData)
-            .then(res => {
-              console.log(res);
-              dispatch(authSuccess(res.data))
-            })
-            .catch(err =>{
-                console.log(err);
-                dispatch(authFailed(err))
-            })
-
+export const auth = (email, password, isSignup) => {
+  return dispatch => {
+    dispatch(authStart());
+    const authData = {
+      email: email,
+      password: password,
+      returnSecureToken: true
+    };
+    let url =
+      "https://www.googleapis.com/identitytoolkit/v3/relyingparty/signupNewUser?key=AIzaSyBGPw3AGtZmEaTyy2GuTr5hjlHwlO_jDew";
+    if (!isSignup) {
+      url =
+        "https://www.googleapis.com/identitytoolkit/v3/relyingparty/verifyPassword?key=AIzaSyBGPw3AGtZmEaTyy2GuTr5hjlHwlO_jDew";
     }
+    axios
+      .post(url, authData)
+      .then(res => {
+        console.log(res);
+        dispatch(authSuccess(res.data.idToken, res.data.localId));
+      })
+      .catch(err => {
+        console.log(err);
+        dispatch(authFailed(err));
+      });
+  };
 };
